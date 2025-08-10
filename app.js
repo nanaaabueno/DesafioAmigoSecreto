1da925ee1f0dc4d271cd98c580fff344593add40
Nayara Bueno <nayaraaabueno@yahoo.com.br>
2025-08-10 20:41:55 -0300

    Implantação das funções adicionarAmigo() e limparCampo()

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+//Desafio Amigo Secreto
+
+let listaAmigos = [];
+console.log(listaAmigos);
+
+
+function adicionarAmigo() {
+    let amigo = document.querySelector('input').value;
+    
+    if(amigo ==''){
+        alert("Por favor, insira um nome.");
+    } else {
+        if (listaAmigos.includes(amigo)) {
+            alert("Você já incluiu este amigo, digite o próximo nome.");
+        } else {
+            listaAmigos.push(amigo);
+            console.log(listaAmigos);
+    }
+    
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';    
+
+    for (let i = 0; i < listaAmigos.length; i++) {
+        let lis = document.createElement('li');
+        lis.innerHTML = listaAmigos[i];
+        lista.appendChild(lis);
+    }
+    
+    limparCampo();
+
+}
+}
+
+function limparCampo() {
+    amigo = document.querySelector('input');
+    amigo.value = '';
+}
+
+
+
